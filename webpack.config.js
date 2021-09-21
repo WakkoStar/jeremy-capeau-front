5d378f3578f008e282ea96bd3b8174f2359aca9a
hugo.delpia <hugo.delpia@canal-plus.com>
2021-09-21 20:45:33 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-      ],
-    },
-  };
-  Finally run webpack via your preferred method.
-  
-  Reso
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
