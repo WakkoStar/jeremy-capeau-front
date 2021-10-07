ab0bcef838055fa2082b6442aeba38fb6be6518e
WakkoStar <jethug.hugo@orange.fr>
2021-10-07 16:13:09 +0200

    wip page config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.scss';
+import WipPage from './wip';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <WipPage/>
 }
 
 export default MyApp;
---
+++ pages/wip.js
@@ -0,0 +1,11 @@
+
+import styles from "../styles/Home.module.scss";
+
+export default function WipPage() {
+    return (
+            <div className={styles.wip}>
+                <h1>Le site Jérémy Capeau est en construction</h1>
+                <h2>On revient bientôt :)</h2>
+            </div>
+    )
+}
